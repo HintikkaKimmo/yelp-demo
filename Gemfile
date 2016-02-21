b9d69cb5694e572a1322eeb4f71194e1676ad94f
HintikkaKimmo <hintikkakimmo@gmail.com>
2016-02-21 12:20:20 +0000

    Added file uploads with carriewave, s3 location, figaro for dotfiles

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
+gem 'carrierwave'
+gem 'fog'
+gem 'figaro'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
---
+++ config/initializers/CarrierWave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['aws_access_key_id'],                        # required
+    aws_secret_access_key: ENV['aws_secret_access_key'],                        # required
+    # region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+    # host:                  's3.example.com',             # optional, defaults to nil
+    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = ENV['config.fog_directory']                          # required
+  # config.fog_public     = false                                        # optional, defaults to true
+  # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+end
