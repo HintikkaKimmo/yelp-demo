6494e57ab72dd910644bdac71dada51dda901e37
HintikkaKimmo <hintikkakimmo@gmail.com>
2016-02-21 13:21:20 +0000

    Adding minimagick image resizing

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'carrierwave'
 gem 'fog'
 gem 'figaro'
+gem 'mini_magick'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
--- config/initializers/CarrierWave.rb
+++ config/initializers/CarrierWave.rb
@@ -3,11 +3,11 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV['aws_access_key_id'],                        # required
     aws_secret_access_key: ENV['aws_secret_access_key'],                        # required
-    # region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+    region:                ENV['region'],                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = ENV['config.fog_directory']                          # required
+  config.fog_directory  = ENV["config.fog_directory"]                          # required
   # config.fog_public     = false                                        # optional, defaults to true
   # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
