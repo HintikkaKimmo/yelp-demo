41655d1906d371df56c4643c52cda99c6c96a4df
HintikkaKimmo <hintikkakimmo@gmail.com>
2016-02-23 21:50:31 +0000

    link reviews with restaurants

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :users
+  belongs_to :restaurants
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222230800) do
+ActiveRecord::Schema.define(version: 20160223213200) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -26,9 +26,10 @@ ActiveRecord::Schema.define(version: 20160222230800) do
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.text     "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "user_id"
+    t.integer  "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
